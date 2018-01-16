9dfc4577d296cf353553ec0379cb8fa081e705f1
Gillis Robbe <robbe.gillis@immalle.eu>
2018-01-16 09:01:37 +0100

    Add project files.

---
+++ TryParseGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TryParseGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
